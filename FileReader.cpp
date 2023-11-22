e067c0fa6f7c3458d15fcd3a49dddf2d2c8dbf2d
Maquereau2 <macrorush@gmail.com>
2023-11-22 18:18:13 +0100

    update : file reader 🫡

--- FileReader.cpp
+++ FileReader.cpp
@@ -2,6 +2,7 @@
 
 
 void FileReader::readFile(std::string path) {
+    bool isList;
     std::fstream newfile;
     newfile.open(path, std::ios::in);
     int counter = 0;
@@ -13,22 +14,37 @@ void FileReader::readFile(std::string path) {
             if (isOnlyCommas(tp)) {
                 _sizeHeight.push_back(0);
                 _modelStats.resize(_modelStats.size() + 1);
-                counter++;
+                //counter++;
             }
             else {
+                counter = 0;
+                isList = false;
                 std::stringstream ss(tp);
                 std::string value;
                 _sizeHeight.back()++; // Increment the size for the current row
 
                 // Resize _modelStats before accessing its elements
                 _modelStats.back().resize(_sizeHeight.back());
+                size_t pos = tp.find(',');
+                if (pos != std::string::npos && pos + 1 < tp.length() && tp[pos + 1] == '"') {
+                    isList = true;
+                    std::cout << "Double quote found after the first comma in line " << counter << std::endl;
+                }
 
                 while (getline(ss, value, ',')) {
                     try {
                         // Convert the string value to a double and add to _modelStats
-                        _modelStats.back()[_sizeHeight.back() - 1].resize(_modelStats.back()[_sizeHeight.back() - 1].size() + 1);
-                        //_modelStats.back()[_sizeHeight.back() - 1][_modelStats.back()[_sizeHeight.back() - 1].size() - 1].push_back(std::stod(value));
-                        _modelStats.back()[_sizeHeight.back() - 1][_modelStats.back()[_sizeHeight.back() - 1].size() - 1].push_back(std::stod(value));
+                        if (isList) {
+                            _modelStats.back()[_sizeHeight.back() - 1].resize(_modelStats.back()[_sizeHeight.back() - 1].size() + 1);
+                            /*_modelStats.back()[_sizeHeight.back() - 1][_modelStats.back()[_sizeHeight.back() - 1].size() - 1].resize(_modelStats.back()[_sizeHeight.back() - 1].size() + 1); */
+                            _modelStats.back()[_sizeHeight.back() - 1][counter / 5].push_back(std::stod(value));
+                        }
+                        else {
+                            _modelStats.back()[_sizeHeight.back() - 1].resize(_modelStats.back()[_sizeHeight.back() - 1].size() + 1);
+                            //_modelStats.back()[_sizeHeight.back() - 1][_modelStats.back()[_sizeHeight.back() - 1].size() - 1].push_back(std::stod(value));
+                            _modelStats.back()[_sizeHeight.back() - 1][counter % 3].push_back(std::stod(value));
+                        }
+                        counter++;
                     }
                     catch (const std::invalid_argument& e) {
                         std::cerr << "Error converting string to double: " << e.what() << std::endl;
@@ -39,16 +55,16 @@ void FileReader::readFile(std::string path) {
         }
 
         newfile.close();
-        for (size_t i = 0; i < _modelStats.size(); ++i) {
-            for (size_t j = 0; j < _modelStats[i].size(); ++j) {
-                for (size_t k = 0; k < _modelStats[i][j].size(); ++k) {
-                    for (size_t l = 0; l < _modelStats[i][j][k].size(); ++l) {
-                        std::cout << "_modelStats[" << i << "][" << j << "][" << k << "][" << l << "] = "
-                            << _modelStats[i][j][k][l] << std::endl;
-                    }
-                }
-            }
-        }
+        //for (size_t i = 0; i < _modelStats.size(); ++i) {
+        //    for (size_t j = 0; j < _modelStats[i].size(); ++j) {
+        //        for (size_t k = 0; k < _modelStats[i][j].size(); ++k) {
+        //            for (size_t l = 0; l < _modelStats[i][j][k].size(); ++l) {
+        //                std::cout << "_modelStats[" << i << "][" << j << "][" << k << "][" << l << "] = "
+        //                    << _modelStats[i][j][k][l] << std::endl;
+        //            }
+        //        }
+        //    }
+        //}
     }
     else {
         std::cerr << "Error opening file: " << path << std::endl;
