fb8817d0bd0a3ea5a5da00db32f5d22b5556dc6d
ManaIron <117281679+ManaIron@users.noreply.github.com>
2023-11-23 10:33:04 +0100

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -1,15 +1,17 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-#include"GameObject.h
+#include "GameObject.h"
 
-class Enemy : public GameObject{
+class Enemy : public GameObject {
 private:
-    int maxHp;
-    int Hp;
+    int maxHealth;
+    int currentHealth;
 
 public:
-    Enemy(int maxHp);
+    Enemy(float radius, float x, float y, float speed, int maxHealth);
+    Enemy();
+    float getRadius();
     void takeDamage(int damage);
     bool isAlive() const;
 };
