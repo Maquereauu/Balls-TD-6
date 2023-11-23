ef1421913119eb7a94a99d7d60e5cced2868573c
ManaIron <117281679+ManaIron@users.noreply.github.com>
2023-11-23 10:32:44 +0100

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,15 +1,23 @@
 #include "Enemy.h"
 
-Enemy::Enemy(int maxHp)
-    : maxHp(maxHp), Hp(maxHp) {}
+Enemy::Enemy(float radius, float x, float y, float speed, int maxHealth)
+    : GameObject(radius, x, y, speed, -1), maxHealth(maxHealth), currentHealth(maxHealth) {
+    setVector(0.f, 0.f);
+}
+
+Enemy::Enemy() : Enemy(15.f, 700.f, 500.f, 180.f, 100) {}; // Exemple avec 100 comme santé maximale
+
+float Enemy::getRadius() {
+    return _sizeX / 2;
+}
 
 void Enemy::takeDamage(int damage) {
-    Hp -= damage;
-    if (Hp < 0) {
-        Hp = 0;
+    currentHealth -= damage;
+    if (currentHealth < 0) {
+        currentHealth = 0;
     }
 }
 
 bool Enemy::isAlive() const {
-    return Hp > 0;
+    return currentHealth > 0;
 }
